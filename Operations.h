f569962bc0dcc3938e42009912e3e8e074d81ac7
123pik1 <s197736@student.pg.edu.pl>
2024-03-16 20:14:10 +0100

    first commit

---
+++ Operations.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "List.h"
+
+class Operations
+{
+public:
+    int sum(List *stack);
+    int sub(List *stack);
+    int mul(List *stack);
+    int div(List *stack);
+    int ifing(List *stack);
+    int min(List *stack);
+};
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "List.h"
+#include "Operations.h"
+
+using namespace std;
+
+
+int main()
+{
+
+}
